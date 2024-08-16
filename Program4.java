9322ea48795a97f3192ca06f85ec252bbc6d962a
arnavpati-27 <arnav.p4ti@gmail.com>
2024-08-16 14:25:00 -0700

    Update Program4.java
    I wrote the solution

--- Program4.java
+++ Program4.java
@@ -1,10 +1,25 @@
-//Youre on your own!!
-//Jk ask for help if you need it (but also look at previous programs!!)
+import java.util.Scanner;
 
-
-
-//Paste console output below:
-/*
-
-
-*/
+public class program4
+{
+    public static void main(String[] args){
+        Scanner s1 = new Scanner(System.in);
+        int one, two, three, four;
+        
+        System.out.println("Enter a 3 digit number");
+        one = s1.nextInt();
+        System.out.println("Enter a 3 digit number");
+        two = s1.nextInt();
+        System.out.println("Enter a 3 digit number");
+        three = s1.nextInt();
+        System.out.println("Enter a 3 digit number");
+        four = s1.nextInt();
+        
+        int sum = one + two + three + four;
+        System.out.println("The sum of the four numbers is " + sum);
+        
+        double average = sum/4.0;
+        System.out.println("The average of the four numbers is " + average);
+    }
+       
+}
